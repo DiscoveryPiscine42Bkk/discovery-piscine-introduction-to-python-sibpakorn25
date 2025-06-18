a5493e157da3be86d6c7b6269cc2503ac7a6b557
sibpakorn25 <Sibpakornza@gmail.com>
2025-06-18 04:26:33 +0000

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = int(input("Enter"))
+if number < 0:
+    print("this number is negative.")
+elif number > 0:
+    print("this number is positive.")
+else:
+    print("This number is both positive and negative")
+    
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "python is awesome"
+prompt = input()
+if prompt == password"password"
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
